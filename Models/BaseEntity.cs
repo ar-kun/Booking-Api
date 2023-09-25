23f3c3c16eef6ddc677dd35214de273a857f838a
Adam Ramdan <arshinks12@gmail.com>
2023-09-25 13:53:26 +0700

    Ft: first commit

---
+++ Models/BaseEntity.cs
@@ -0,0 +1,8 @@
+namespace BookingApp.Models;
+
+public abstract class BaseEntity
+{
+  public Guid Guid { get; set; }
+  public DateTime CreatedDate { get; set; }
+  public DateTime ModifiedDate { get; set; }
+}
